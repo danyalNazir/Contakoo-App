a0f711df21d6942d60ee71ec3896535b44a7651a
Danyal <Daniraj107@gmail.com>
2022-05-20 01:00:20 -0700

    Send Message Activity

--- app/src/main/java/com/example/contakoo/SendMessageActivity.java
+++ app/src/main/java/com/example/contakoo/SendMessageActivity.java
@@ -2,7 +2,13 @@ package com.example.contakoo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SendMessageActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class SendMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
+
+        Button buttonMessage=(Button)  findViewById(R.id.buttonMessage);
+        Button buttonBack=(Button) findViewById(R.id.buttonBackMessage);
+        EditText editTextMessage=(EditText) findViewById(R.id.editTextMessage);
+
+
+        buttonMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("smsto:" + editTextMessage.getText().toString());
+                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                startActivity(intent);
+            }
+        });
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SendMessageActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
